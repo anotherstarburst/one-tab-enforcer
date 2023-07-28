123618950aa485a1bfb4f4eb3cf550f54f557220
Aaron <aaron@webappio.com>
2023-07-28 17:27:58 +0100

    Bump prod build.

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,7 +1,17 @@
-declare function theEnforcer({ appID, timeout, warningQuerySelector, warningMarkup }: {
+/**
+ * Interface defining the options that can be passed to the DuplicateTabEnforcer function.
+ */
+interface EnforcerOptions {
     appID?: string;
     timeout?: number;
     warningQuerySelector?: string;
     warningMarkup?: string;
-}, functionToRun: Function): void;
-export default theEnforcer;
+}
+/**
+ * Ensure that the provided function runs only once in a unique tab.
+ *
+ * @param {EnforcerOptions} options Configuration options.
+ * @param {Function} functionToRun The function to run when no duplicate tab is detected.
+ */
+declare function DuplicateTabEnforcer(options: EnforcerOptions, functionToRun: Function): void;
+export default DuplicateTabEnforcer;
